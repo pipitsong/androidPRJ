13ac5610121b8c349e05641ef3b23fb308aba00f
ali10Top <email.fivemin@gmail.com>
2022-01-01 12:11:09 +0330

    add intetnt show another activity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,16 +2,28 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button bt ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
-        TextView tv = findViewById(R.id.textView2);
-        tv.setText("H\n12\n66");
+        setContentView(R.layout.activity_main);
+        bt = findViewById(R.id.button);
+        bt.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intetnt = new Intent(MainActivity.this, MainActivity2.class );
+        startActivity(intetnt);
     }
 }
